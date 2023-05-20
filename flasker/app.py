281bf84bcf1883d00286024cc6dc3e7fa8e0bc69
yuraskivka <yuraskivka@gmail.com>
2023-05-20 15:38:06 +0300

    How To Use Messages With Flask - Flask Fridays #6

--- flasker/app.py
+++ flasker/app.py
@@ -95,6 +95,7 @@ def name():
     if form.validate_on_submit():
         name = form.name.data
         form.name.data = ''
+        flash('Form Submit Successfully!')
     return render_template("name.html",
                            name=name,
                            form=form)
